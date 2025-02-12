14b8e17d3f4af51956ef25d67914d817bca816ae
contrerascs <60404702+contrerascs@users.noreply.github.com>
2025-02-11 22:47:16 -0600

    Get hash file

--- Main.py
+++ Main.py
@@ -1,3 +1,13 @@
 import os
 import hashlib
 
+def hash_file(filename):
+    h = hashlib.md5()
+    with open(filename, 'rb') as file:
+        while chunk := file.read(8192):
+            h.update(chunk)
+    return h.hexdigest()
+
+archivo = 'files/texto.txt'
+
+print(hash_file(archivo))
