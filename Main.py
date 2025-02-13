329a556d74d81b0de0218af6272765600af79702
contrerascs <60404702+contrerascs@users.noreply.github.com>
2025-02-12 23:20:27 -0600

    Find and delete duplicates

--- Main.py
+++ Main.py
@@ -8,6 +8,25 @@ def hash_file(filename):
             h.update(chunk)
     return h.hexdigest()
 
-archivo = 'files/texto.txt'
+def find_duplicates(folder):
+    hashes = {}
+    for dirpath, _, filenames in os.walk(folder):
+        #print(filenames)
+        #print(dirpath)
+        for f in filenames:
+            full_path = os.path.join(dirpath, f)
+            file_hash = hash_file(full_path)
+            #print(full_path)
+            #print(file_hash)
+            if file_hash in hashes:
+                print(f'duplicates were found: {full_path}') == {hashes[file_hash]}
+                delete = input(f'¿Do you want delete duplicated files {full_path}? (s/n): ').strip().lower()
+                if delete == 's':
+                    os.remove(full_path)
+                    print(f'File {full_path} deleted.')
+                else:
+                    print(f'Files {full_path} not deleted.')
+            else:
+                hashes[file_hash] = full_path
 
-print(hash_file(archivo))
+find_duplicates('files')
