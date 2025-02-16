fb9bb4fc6d03f396895fee11f20fe03a7f720ad8
contrerascs <60404702+contrerascs@users.noreply.github.com>
2025-02-15 18:50:57 -0600

    Delete duplicates with python

--- Delete_Duplicates.py
+++ Delete_Duplicates.py
@@ -10,23 +10,22 @@ def hash_file(filename):
 
 def find_duplicates(folder):
     hashes = {}
+    duplicates = {}
     for dirpath, _, filenames in os.walk(folder):
         #print(filenames)
         #print(dirpath)
         for f in filenames:
             full_path = os.path.join(dirpath, f)
             file_hash = hash_file(full_path)
-            #print(full_path)
-            #print(file_hash)
             if file_hash in hashes:
-                print(f'duplicates were found: {full_path}') == {hashes[file_hash]}
-                delete = input(f'¿Do you want delete duplicated files {full_path}? (s/n): ').strip().lower()
-                if delete == 's':
-                    os.remove(full_path)
-                    print(f'File {full_path} deleted.')
-                else:
-                    print(f'Files {full_path} not deleted.')
+                duplicates.append((full_path), hashes[file_hash])
             else:
                 hashes[file_hash] = full_path
+    return duplicates
 
-find_duplicates('files')
+def delete_files(filepath):
+    try:
+        os.remove(filepath)
+        return True
+    except Exception as e:
+        return False
